3f7c83a8f74f64e9291121a630d5e04ec2ec78d3
Rishikesh-singh-hub <rs1848962@gmail.com>
2025-12-02 17:58:56 +0530

    small change in Services.java

--- src/main/java/com/rishikesh/app/service/Services.java
+++ src/main/java/com/rishikesh/app/service/Services.java
@@ -6,8 +6,6 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @Service
